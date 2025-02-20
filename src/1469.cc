226725e5d9d0e9f8c2b4912bd9e8ef6f63d2144c
Kiwoong Nam <nkw1725@gmail.com>
2025-02-20 15:40:57 +0900

    1469 ac

--- src/1469.cc
+++ src/1469.cc
@@ -12,18 +12,21 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pp;
 
-int n, cur;
-vector<char> x;
+#define EMPTY -1
+
+int n;
+vector<int> x;
 vector<bool> used;
-vector<char> ss;
+vector<int> ss;
 
 void dfs(int level) {
+  int cur;
   if (level == n) {
     for (auto s : ss)
       cout << s << ' ';
     exit(0);
   }
-  for (cur = level; cur < 2 * n && ss[cur] != 0; cur++)
+  for (cur = level; cur < 2 * n && ss[cur] != EMPTY; cur++)
     ;
   if (cur == 2 * n)
     return;
@@ -32,23 +35,24 @@ void dfs(int level) {
     if (used[i])
       continue;
     ss[cur] = x[i];
-    if ((x[i] + cur) >= (2 * n)) {
-      cout << cur << " is cur, " << x[i] << " is x[i]\n";
-      cout << 2 * n << endl;
-      ss[cur] = 0;
+    if ((x[i] + cur + 1) >= (2 * n)) {
+      ss[cur] = EMPTY;
       continue;
     }
-    cout << "HELP" << endl;
+    if (ss[x[i] + cur + 1] != EMPTY) {
+      ss[cur] = EMPTY;
+      continue;
+    }
 
-    ss[x[i] + cur] = x[i];
+    ss[x[i] + cur + 1] = x[i];
     used[i] = true;
-    for (auto s : ss)
-      cout << s << ' ';
-    cout << endl;
+    // for (auto s : ss)
+    //   cout << (s == EMPTY ? '*' : (char)(s + 48)) << ' ';
+    // cout << endl;
     dfs(level + 1);
     used[i] = false;
-    ss[cur] = 0;
-    ss[x[i] + cur] = 0;
+    ss[cur] = EMPTY;
+    ss[x[i] + cur + 1] = EMPTY;
   }
 }
 
@@ -57,10 +61,11 @@ int main() {
   cin >> n;
 
   x.resize(n);
-  ss.resize(2 * n);
+  ss.resize(2 * n, EMPTY);
   used.resize(n);
   for (int i = 0; i < n; i++)
     cin >> x[i];
+  sort(x.begin(), x.end());
   dfs(0);
 
   cout << -1;
