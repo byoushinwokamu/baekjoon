185877d5a1ea27d60bb0b061d6eacd47ae181014
Kiwoong Nam <nkw1725@gmail.com>
2025-03-16 20:16:11 +0900

    30969 ac

--- src/30969.cc
+++ src/30969.cc
@@ -15,17 +15,24 @@ typedef pair<int, int> pp;
 int main() {
   cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
   int n, jinju;
-  cin >> n;
-  vector<int> fee(n);
+  ll fee;
   string s;
+  vector<int> road(1002);
+
+  cin >> n;
   for (int i = 0; i < n; i++) {
-    cin >> s >> fee[i];
+    cin >> s >> fee;
+    if (fee > 1000)
+      road[1001]++;
+    else
+      road[fee]++;
     if (s == "jinju")
-      jinju = fee[i];
+      jinju = fee;
   }
-  sort(fee.begin(), fee.end());
-  auto it = find(fee.begin(), fee.end(), jinju);
-  cout << jinju << '\n' << (int)(fee.end() - it - 1);
+  int sum = 0;
+  for (int i = jinju + 1; i <= 1001; i++)
+    sum += road[i];
+  cout << jinju << '\n' << sum;
 
   return 0;
 }
